a54bf04f97f7b807d889814884962e8f42ef7249
Juanjonieto05 <juanjonieto05@gmail.com>
2025-07-01 17:39:34 -0600

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "40px" }}>
+      <h1>¡Hola desde Nomigate!</h1>
+      <p>Tu aplicación está funcionando correctamente 🚀</p>
+    </div>
+  );
+};
+
+export default App;
